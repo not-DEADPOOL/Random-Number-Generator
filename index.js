96371f0e7d258e11ac018df2f0d7e4bd44b946f4
DEAD POOL <78198419+not-DEADPOOL@users.noreply.github.com>
2023-06-22 18:01:23 +0200

    Added number roller
    with promt asking how many maximum cifres do you want

---
+++ index.js
@@ -0,0 +1,25 @@
+try {
+    var val = Number(window.prompt("How many maximum cifres will the number have?"));
+}
+catch (error) {
+    var val = 1;
+}
+val = getVal();
+setInterval(setTime, 10);
+function setTime() {
+    document.getElementById("numb").innerHTML = getNum();
+}
+
+function getNum() {
+    // console.log(val);
+    let shift = Math.random() * 2;
+    return Math.floor(Math.random() * val);
+}
+function getVal() {
+    val < 1 ? 1 : val;
+    start = 10;
+    for (let i = 1; i < val; i++) {
+        start *= 10;
+    }
+    return start;
+}
